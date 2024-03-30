d65561e6a65aabbf1718944cde368d93baaf2dea
Солохин Алексей Викторович <a.solokhin@kmd24.com>
2024-03-30 19:14:26 +0300

    country detail modal

---
+++ src/pages/map/components/country-detail-modal/country-detail-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-detail-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-detail-modal.component.html',
+  styleUrl: './country-detail-modal.component.scss'
+})
+export class CountryDetailModalComponent {
+
+}
